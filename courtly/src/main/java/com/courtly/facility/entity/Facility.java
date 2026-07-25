7baaa264b24c234c44344880050425bee91b6f56
Alberto Garcia <algartxi@gmail.com>
2026-07-25 12:50:33 +0200

    feat: Facility entity and migration created

---
+++ courtly/src/main/java/com/courtly/facility/entity/Facility.java
@@ -0,0 +1,100 @@
+package com.courtly.facility.entity;
+
+import java.time.LocalTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "facilities")
+public class Facility {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 200)
+    private String address;
+
+    @Column(length = 500)
+    private String description;
+
+    @Column(name = "opening_time", nullable = false)
+    private LocalTime openingTime;
+
+    @Column(name = "closing_time", nullable = false)
+    private LocalTime closingTime;
+
+    public Facility() {
+    }
+
+    public Facility(String name, String address, String description, LocalTime openingTime, LocalTime closingTime) {
+        this.name = name;
+        this.address = address;
+        this.description = description;
+        this.openingTime = openingTime;
+        this.closingTime = closingTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    } 
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalTime getOpeningTime() {
+        return openingTime;
+    }
+
+    public void setOpeningTime(LocalTime openingTime) {
+        this.openingTime = openingTime;
+    }
+
+    public LocalTime getClosingTime() {
+        return closingTime;
+    }
+
+    public void setClosingTime(LocalTime closingTime) {
+        this.closingTime = closingTime;
+    }
+
+    public String toString() {
+        return "Facility{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", description='" + description + '\'' +
+                ", openingTime=" + openingTime +
+                ", closingTime=" + closingTime +
+                '}';
+    }
+}
